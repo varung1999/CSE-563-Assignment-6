a174217703e15bf85e10586d31162f1753413c1b
Varun Gollapalli <gvarun1999@gmail.com>
2022-04-26 23:49:06 -0700

    US#3-Task#12 Create Save option on menubar
    US#3-Task#12 Create Save option on menu bar

--- MainFrame.java
+++ MainFrame.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
 
@@ -8,16 +7,87 @@ public class MainFrame extends JFrame {
 
     private static final int DEFAULT_WINDOW_HEIGHT = 500;
     private static final int DEFAULT_WINDOW_WIDTH = 600;
+    public static String temp;
+    public static int numberOfRandomDots;
+    WorkSpace workSpace;
+    WorkSpacePanel workSpacePanel;
+    
     
     public MainFrame(){
         super();// set title over here
+        
+        workSpace = new WorkSpace();
+        workSpacePanel = new WorkSpacePanel(workSpace);
+
         setLayout(new BorderLayout());
 
-        WorkSpace workSpace = new WorkSpace();
-        WorkSpacePanel workSpacePanel = new WorkSpacePanel(workSpace);
         add(workSpacePanel,BorderLayout.CENTER);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu FileMenu = new JMenu("File");
+        
+
+        
+
+        JLabel LogLabel = new JLabel("",JLabel.CENTER);
+
+        
+        JMenuItem SaveMenuItem = new JMenuItem("Save File");
+        
+        FileMenu.add(SaveMenuItem);
+       
+        //adding menu to menu bar
+        menuBar.add(FileMenu);
+       
+
+
+        //action listener to Clear Canvas
+        ClearCanvasItem.addActionListener(ev->{
+
+            workSpace.pointList.clear();
+            workSpacePanel.repaint();
+
+        });
+        
+
+        //action listener to Save Menu
+        SaveMenuItem.addActionListener(ev->{
+            File selectedFile = displayFileSaveDialog();
+            if(selectedFile!= null)
+            {
+                try{ // send the selected file to required place.
+                
+                }
+                catch(IOException e)
+                {
+                    String message = String.format("Failed To Save Data To File\nException: %s", e);
+                    JOptionPane.showMessageDialog(this, message);
+                }
+            }
+            LogLabel.setText("Save option from File menu is clicked");
+        });
+
+        //action listener to Load Menu
+       
+
+        //action listener to random generator
+       
+        
+        //adding to the frame
+        add(menuBar,BorderLayout.NORTH);
+        //adding log label to the frame
+       
+        
     }
 
+   
+
+    //private method for random dot generator to generate random dots.
+
+    
+
+
+    //main method
     public static void main(String args[])
     {
         MainFrame mainFrame = new MainFrame();
